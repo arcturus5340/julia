beb430c8f33f862bd136bf170b77b1a16c4f556b
thmbele <th.mbele@gmail.com>
2020-08-07 07:05:14 +0500

    Create separate html-files for the forms Refactor index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,95 +1,122 @@
 login =
-    "<h2>Hi, I'm Julia.</h2>\n" +
-    "<div class=\"login-info\">Tell me your username and password.</div>\n" +
-    "<input type=\"text\" name=\"Username\" id=\"username\" placeholder=\"Username\">\n" +
-    "<input type=\"password\" name=\"Password\" id=\"password\" placeholder=\"Password\" autocomplete=\"on\">\n" +
-    "<button class=\"major-button\" type=\"button\" name=\"Login\" value=\"Login\">Login</button>\n" +
-    "<div class=\"unusual-actions\">\n" +
-    "    <div class=\"reset-password\"><span id=\"reset-password\">I forgot my password.</a></div>\n" +
-    "    <div class=\"unusual-actions-sep\">||</div>\n" +
-    "    <div class=\"registration\"><span id=\"registration\">We're not acquainted.</a></div>\n" +
-    "</div>\n" +
-    "<a href=\"https://github.com/arcturus5340/julia\" class=\"social-circle icoGitHub\" title=\"GitHub\"><i class=\"fab fa-github\"></i></a>\n"
+    "<div class=\"login-form\">\n" +
+    "    <h2>Hi, I'm Julia.</h2>\n" +
+    "    <div class=\"login-info\">Tell me your username and password.</div>\n" +
+    "    <label for=\"username\"></label>\n" +
+    "    <input type=\"text\" name=\"Username\" id=\"username\" placeholder=\"Username\">\n" +
+    "    <label for=\"password\"></label>\n" +
+    "    <input type=\"password\" name=\"Password\" id=\"password\" placeholder=\"Password\" autocomplete=\"on\">\n" +
+    "    <button class=\"major-button\" type=\"button\" name=\"Login\" value=\"Login\">Login</button>\n" +
+    "    <div class=\"unusual-actions\">\n" +
+    "        <div class=\"reset-password\"><span id=\"reset-password\">I forgot my password.</span></div>\n" +
+    "        <div class=\"unusual-actions-sep\">||</div>\n" +
+    "        <div class=\"registration\"><span id=\"registration\">We're not acquainted.</span></div>\n" +
+    "    </div>\n" +
+    "    <a href=\"https://github.com/arcturus5340/julia\" class=\"social-circle icoGitHub\" title=\"GitHub\"><i class=\"fab fa-github\"></i></a>\n" +
+    "</div>"
 
 reset_password =
-    "<h2>Oh, let me help you.</h2>\n" +
-    "<div class=\"login-info\">Tell me your username or e-mail.</div>\n" +
-    "<div class=\"login-info\">I'll send you a link to get back into your account.</div>\n" +
-    "<input type=\"text\" name=\"UsernameOrEmail\" id=\"UsernameOrEmail\" placeholder=\"Username or e-mail\">\n" +
-    "<button class=\"major-button\" type=\"button\" name=\"Send\" value=\"Send\" >Send</button>\n" +
-    "<div class=\"back-to-login\">\n" +
-    "    <span id=\"login\">I remembered my password.</a>\n" +
-    "</div>\n" +
-    "<a href=\"https://github.com/arcturus5340/julia\" class=\"social-circle icoGitHub\" title=\"GitHub\"><i class=\"fab fa-github\"></i></a>\n"
+    "<div class='reset-form'>" +
+    "    <h2>Oh, let me help you.</h2>\n" +
+    "    <div class=\"login-info\">Tell me your username or e-mail.</div>\n" +
+    "    <div class=\"login-info\">I'll send you a link to get back into your account.</div>\n" +
+    "    <input type=\"text\" name=\"UsernameOrEmail\" id=\"UsernameOrEmail\" placeholder=\"Username or e-mail\">\n" +
+    "    <button class=\"major-button\" type=\"button\" name=\"Send\" value=\"Send\" >Send</button>\n" +
+    "    <div class=\"back-to-login\">\n" +
+    "        <span id=\"login\">I remembered my password.</a>\n" +
+    "    </div>\n" +
+    "    <a href=\"https://github.com/arcturus5340/julia\" class=\"social-circle icoGitHub\" title=\"GitHub\"><i class=\"fab fa-github\"></i></a>\n" +
+    "</div>"
 
 registration =
-    "<h2>Let's get acquainted!</h2> \n" +
-    "<div class=\"login-info\">I need your future username,</div> \n" +
-    "<div class=\"login-info\">your e-mail & your future password.</div> \n" +
-    "<input type=\"text\" name=\"reg-Username\" id=\"reg-username\" placeholder=\"Username\"> \n" +
-    "<input type=\"text\" name=\"reg-E-mail\" id=\"reg-e-mail\" placeholder=\"E-mail\"> \n" +
-    "<input type=\"password\" name=\"reg-Password\" id=\"reg-password\" placeholder=\"Password\" autocomplete=\"on\"> \n" +
-    "<button class=\"major-button\" type=\"button\" name=\"Login\" value=\"Login\">Sign Up</button> \n" +
-    "<div class=\"back-to-login\">\n" +
-    "    <span id=\"login\">We're already acquainted.</a>\n" +
-    "</div>\n" +
-    "<a href=\"https://github.com/arcturus5340/julia\" class=\"social-circle icoGitHub\" title=\"GitHub\"><i class=\"fab fa-github\"></i></a>"
-
-main_form = $('#main-form')
-
-
-$('#reset-password').on('click', function () {
-    main_form.css('transform', 'translateY(65%)');
-    main_form.html(reset_password);
-})
+    "<div class='registration-form'>" +
+    "    <h2>Let's get acquainted!</h2> \n" +
+    "    <div class=\"login-info\">I need your future username,</div> \n" +
+    "    <div class=\"login-info\">your e-mail & your future password.</div> \n" +
+    "    <input type=\"text\" name=\"reg-Username\" id=\"reg-username\" placeholder=\"Username\"> \n" +
+    "    <input type=\"text\" name=\"reg-E-mail\" id=\"reg-e-mail\" placeholder=\"E-mail\"> \n" +
+    "    <input type=\"password\" name=\"reg-Password\" id=\"reg-password\" placeholder=\"Password\" autocomplete=\"on\"> \n" +
+    "    <button class=\"major-button\" type=\"button\" name=\"Login\" value=\"Login\">Sign Up</button> \n" +
+    "    <div class=\"back-to-login\">\n" +
+    "        <span id=\"login\">We're already acquainted.</a>\n" +
+    "    </div>\n" +
+    "    <a href=\"https://github.com/arcturus5340/julia\" class=\"social-circle icoGitHub\" title=\"GitHub\"><i class=\"fab fa-github\"></i></a>" +
+    "</div>"
 
 
-$('#registration').on('click', function () {
-    main_form.css('transform', 'translateY(30%)');
-    main_form.html(registration);
-})
-
-
-$(document).on('click', '#login',function () {
-    main_form.css('transform', 'translateY(50%)');
-    main_form.html(login);
-    $(document).on('click', '#reset-password',function () {
-        main_form.css('transform', 'translateY(65%)');
+function reset_password_handler () {
+    main_form = $('#main-form')
+    $(document).on('click', '#reset-password', function () {
         main_form.html(reset_password);
     })
-})
+}
 
-
-$(document).on('click', '#login',function () {
-    main_form.css('transform', 'translateY(50%)');
-    main_form.html(login);
-    $(document).on('click', '#registration',function () {
-        main_form.css('transform', 'translateY(30%)');
+function registration_handler () {
+    main_form = $('#main-form')
+    $(document).on('click', '#registration', function () {
         main_form.html(registration);
     })
-})
+}
+
+
+function login_handler () {
+    $(document).on('click', '#login', function () {
+        main_form = $('#main-form')
+        main_form.html(login);
+        reset_password_handler()
+        registration_handler()
+    })
+}
+
+
+reset_password_handler()
+registration_handler()
+login_handler()
 
 
 $(document).keyup(function(e) {
     if (e.keyCode === 13) {
         $('.major-button').click();
-    }
-
-    if (e.keyCode === 27) {
-        main_form.css('transform', 'translateY(50%)');
-        main_form.html(login);
-        $(document).on('click', '#reset-password',function () {
-            main_form.css('transform', 'translateY(65%)');
-            main_form.html(reset_password);
-        });
-        $(document).on('click', '#registration',function () {
-            main_form.css('transform', 'translateY(30%)');
-            main_form.html(registration);
-        })
+    } else if (e.keyCode === 27) {
+        $('#login').click()
     }
 })
 
 
-$('.major-button').on('click',function () {
-    console.log("Major button has been clicked.");
+$('#reset-password').on('click', function () {
+    $.ajax({
+        url: '',
+        type: 'POST',
+        data: {
+            action: 'reset',
+            // csrfmiddlewaretoken: csrfmiddlewaretoken, он здесь же не нужен...
+        },
+        success: function(response){
+            console.log(response)
+        },
+        error: function(){
+            console.log('internal error.')
+        },
+    });
+    // Твое задание:
+    // Элемент с id main_form заменить на HTML-код в переменной reset_password
+})
+
+$('#registration').on('click', function () {
+    $.ajax({
+        url: '',
+        type: 'POST',
+        data: {
+            action: 'registration',
+            // csrfmiddlewaretoken: csrfmiddlewaretoken, и здесь тоже не нужен...
+        },
+        success: function(response){
+            console.log(response)
+        },
+        error: function(){
+            console.log('internal error.')
+        },
+    });
+    // Твое задание:
+    // Элемент с id main_form заменить на HTML-код в переменной registration
 })
