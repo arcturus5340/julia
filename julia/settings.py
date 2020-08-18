946b342d9ba41fa651af598c0c42f72c4312d947
arcturus5340 <arcturus5340@gmail.com>
2020-08-18 23:52:10 +0200

    Add new apps to INSTALLED_APPS list

--- julia/settings.py
+++ julia/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth.apps.AuthConfig',
     'contest.apps.ContestConfig',
+    'rating.apps.RatingConfig',
 ]
 
 MIDDLEWARE = [
