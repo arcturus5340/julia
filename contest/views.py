d3f6982b3984b947453c454e6cb35b5a440c1bba
arcturus5340 <arcturus5340@gmail.com>
2020-08-18 23:51:10 +0200

    Add file upload prototype

--- contest/views.py
+++ contest/views.py
@@ -1,14 +1,21 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 
-from contest.models import Task
+from contest.models import Code, Task
 
 
 def index(request):
-    if request.user.is_anonymous:
-        return render(request, 'index.html')
+    # if request.user.is_anonymous:
+    #     return render(request, 'index.html')
 
     context = {
         'Tasks': Task.objects.all(),
     }
+
+    if request.method == 'POST' and request.FILES['code-file']:
+        code = request.FILES['code-file']
+        fs = FileSystemStorage()
+        filename = fs.save(code.name, code)
+        Code.objects.create_by_filename(filename)
 
     return render(request, 'contest.html', context)
