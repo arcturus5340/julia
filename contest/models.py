3d8339e80dc2eb2bfe59332de667ba3b0979654b
arcturus5340 <arcturus5340@gmail.com>
2020-08-20 22:54:20 +0200

    Add new model to contest app
    The new model will be for storing the submitted
    code in the database

--- contest/models.py
+++ contest/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Code(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    file = models.FileField()
 
 
 class Task(models.Model):
