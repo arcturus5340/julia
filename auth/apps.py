b5821a4bac7e0d93a22ed79e3949d1ef1b793128
arcturus5340 <arcturus5340@gmail.com>
2020-08-21 23:56:13 +0200

    Rename auth app label
    The application has been renamed to improve
    the display of tables of this application in the database

--- auth/apps.py
+++ auth/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class AuthConfig(AppConfig):
     name = 'auth'
-    label = 'custom.auth'
+    label = 'custom_auth'
 
     def ready(self):
         import auth.signals
