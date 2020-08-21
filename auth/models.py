c820f35576ab8e7ebca55cc8e0e4eb52e92e50e9
arcturus5340 <arcturus5340@gmail.com>
2020-08-21 23:59:28 +0200

    Add basic model for Activation Keys storage

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ActivationKeys(models.Model):
+    key = models.CharField(
+        max_length=32,
+    )
+    is_password_reset = models.BooleanField(
+        default=False,
+    )
+    is_email_verification = models.BooleanField(
+        default=False,
+    )
