69a862e9f8636528df0156ed352dea55ca8f67ba
arcturus5340 <arcturus5340@gmail.com>
2020-08-21 23:53:58 +0200

    Add reset-password basic view

--- auth/views.py
+++ auth/views.py
@@ -2,6 +2,23 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.http.response import JsonResponse
+from django.conf import settings
+from django.core import mail
+
+from contest.views import index
+
+import smtplib
+
+
+def send_message(subject: str, message: str, user: User):
+    from_email = settings.EMAIL_HOST_USER
+    try:
+        mail.send_mail(subject, message, from_email, [user.email])
+    except smtplib.SMTPException as err:
+        return JsonResponse({
+            'status': 'fail',
+            'message': 'error sending activation email',
+        })
 
 
 def login(request):
@@ -46,6 +63,10 @@ def registration(request):
         pass
 
     return change_to_login_form(request)
+
+
+def reset_password(request):
+    pass
 
 
 def change_to_login_form(request):
