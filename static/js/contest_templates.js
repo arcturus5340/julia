df246a273c4d3180ce54ee4498a8da5b2938c556
thmbele <th.mbele@gmail.com>
2020-09-01 05:32:29 +0500

    Create task_content variable with task_content.html inside

---
+++ static/js/contest_templates.js
@@ -0,0 +1,37 @@
+task_content =
+    "<div class=\"tab-pane fade show active\" id=\"task-${id}-content\" role=\"tabpanel\" aria-labelledby=\"task-${id}-tab\">\n" +
+    "    <div class=\"header\">\n" +
+    "        <h1>\n" +
+    "            ${title} \n" +
+    "        </h1>\n" +
+    "    </div>\n" +
+    "    <table class=\"limits-table\">\n" +
+    "        <tbody>\n" +
+    "        <tr>\n" +
+    "            <td class=\"task-parameter\"><i class=\"fas fa-hourglass\"></i> ${tl} </td>\n" +
+    "            <td class=\"task-parameter\"><i class=\"fas fa-memory\"></i> ${ml} </td>\n" +
+    "        </tr>\n" +
+    "        </tbody>\n" +
+    "    </table>\n" +
+    "    <div class=\"description\">\n" +
+    "        ${content} \n" +
+    "    </div>\n" +
+    "    <table class=\"io-table\">\n" +
+    "        <thead>\n" +
+    "        <tr>\n" +
+    "            <th scope=\"col\"><i class=\"fas fa-arrow-right\"></i> Input </th>\n" +
+    "            <th scope=\"col\"><i class=\"fas fa-arrow-left\"></i> Output </th>\n" +
+    "        </tr>\n" +
+    "        </thead>\n" +
+    "        <tbody>\n" +
+    "        <tr>\n" +
+    "            <td><code>Input Sample</code></td>\n" +
+    "            <td><code>Output Sample</code></td>\n" +
+    "        </tr>\n" +
+    "        <tr>\n" +
+    "            <td><code>Input Sample</code></td>\n" +
+    "            <td><code>Output Sample</code></td>\n" +
+    "        </tr>\n" +
+    "        </tbody>\n" +
+    "    </table>\n" +
+    "</div>"
