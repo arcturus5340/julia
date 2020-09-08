a09162b0f8504a19418aff79c6471710572d60da
arcturus5340 <arcturus5340@gmail.com>
2020-09-08 23:32:54 +0200

    Add custom authentication backend
    It check if users account is verified

---
+++ auth/backends.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.backends import ModelBackend
+
+class VerificationBackend(ModelBackend):
+    def user_can_authenticate(self, user):
+        return user.groups.filter(name='Verified Users').exists() and super().user_can_authenticate(user)
