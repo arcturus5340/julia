6191317fe0a51802e60439686e55534c92b43aac
arcturus5340 <arcturus5340@gmail.com>
2020-09-13 23:23:57 +0200

    Add scheduled tasks

--- auth/apps.py
+++ auth/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthConfig(AppConfig):
     name = 'auth'
     label = 'custom_auth'
+
+    def ready(self):
+        from auth import scheduled_tasks
+        scheduled_tasks.start()
---
+++ auth/scheduled_tasks.py
@@ -0,0 +1,24 @@
+from auth.models import Activation, EmailTemplates
+from django.utils import timezone
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.contrib import auth
+
+
+def delete_old_activation_keys():
+    Activation.objects.filter(created__gt=timezone.now()-timezone.timedelta(days=1)).delete()
+
+
+def delete_old_email_templates():
+    EmailTemplates.objects.filter(created__gt=timezone.now()-timezone.timedelta(days=1)).delete()
+
+
+def delete_unverified_users():
+    User.objects.exclude(groups__name='Verified Users').delete()
+
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(delete_old_activation_keys, 'interval', days=1)
+    scheduler.add_job(delete_old_email_templates, 'interval', days=1)
+    scheduler.add_job(delete_unverified_users, 'interval', days=1)
+    scheduler.start()
