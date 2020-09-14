dd3d61b5ddbe196c640f52323c3769dd771e4dc6
arcturus5340 <arcturus5340@gmail.com>
2020-09-14 15:19:55 +0200

    Add basename for user model

--- auth/urls.py
+++ auth/urls.py
@@ -3,6 +3,6 @@ import auth.views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'users', auth.views.UserViewSet)
+router.register(r'users', auth.views.UserViewSet, basename='user')
 
 urlpatterns = router.urls
