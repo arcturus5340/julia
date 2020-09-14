69caa463565c522dc8163f37aa0195dae70513ad
arcturus5340 <arcturus5340@gmail.com>
2020-09-14 15:18:58 +0200

    Add IsAdmin object permission

--- auth/permissions.py
+++ auth/permissions.py
@@ -1,6 +1,9 @@
 from rest_framework import permissions
 
-
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj == request.user
+
+class IsAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user) and request.user.is_staff
