bc58f39cd03a52d0f8ee34b8194830463b25e41e
arcturus5340 <arcturus5340@gmail.com>
2020-09-16 13:02:01 +0200

    Update the base user username length validation

--- auth/models.py
+++ auth/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
 
 class Activation(models.Model):
     user = models.ForeignKey(
@@ -37,7 +40,16 @@ class EmailTemplates(models.Model):
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(
+        'username',
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[UnicodeUsernameValidator(), MinLengthValidator(3), MaxLengthValidator(32)],
+        error_messages={
+            'unique': 'A user with that username already exists.',
+        },
+    )
 
     class Meta:
         db_table = 'auth_user'
