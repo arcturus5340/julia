31d7c0b5ac2b32d2b592a0b583499d5abfc87d0e
arcturus5340 <arcturus5340@gmail.com>
2021-04-17 16:53:33 +0200

    Add robots.txt

--- julia/urls.py
+++ julia/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 from rest_framework import routers
 import rest_framework_jwt.views
@@ -27,6 +28,7 @@ urlpatterns = [
     path('api/token-refresh', rest_framework_jwt.views.refresh_jwt_token),
 
     path('api/', include(router.urls)),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'))
 
     # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ] + static(settings.CODE_DIR, document_root=settings.CODE_ROOT, view=secure_serve)
